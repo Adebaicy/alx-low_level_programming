e25ad41f6c8a943a220f82d4e05cbf9bb231dd80
Adebaicy <66224641+Adebaicy@users.noreply.github.com>
2022-09-08 23:54:46 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+
 /**
- * main - describe
+ * main - prints the size of various types
  * Return - return
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
