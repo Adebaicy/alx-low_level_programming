96199cbd1435014f482e81bfe510bee2dec1b60b
Adebaicy <66224641+Adebaicy@users.noreply.github.com>
2022-09-09 00:02:12 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * mtain - prints the size of various types
- * Return: 0 if exited properly, non-zero otherwise
+ * main - prints the size of various types
+ * Return: 0 
  */
 int main(void)
 {
