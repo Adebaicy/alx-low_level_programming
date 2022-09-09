eb20ff21b6738a57fd20c7c9393273214c281cf9
Adebaicy <66224641+Adebaicy@users.noreply.github.com>
2022-09-09 20:12:57 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
+
 
 /**
  * main - prints exactly "and that piece of art is useful"
@@ -10,7 +10,7 @@
  * Return: Always 0 (Success)
  */
  
-/* betty style doc for function main goes there */
+
 int main(void)
 {
 	int n;
@@ -23,12 +23,12 @@ int main(void)
   	}
   	else if ( n == 0)
   	{
-    		printf ("%d is zero", n);
+    		printf ("%d is zero\n", n);
   	}
   	else
  	{
-    		printf ("%d is negative", n);
+    		printf ("%d is negative\n", n);
   	}
-	/* your code goes there */
+	
 	return (0);
 }
