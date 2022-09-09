70e4dd5d501572b262dce47c9951f9db51f9f438
Adebaicy <66224641+Adebaicy@users.noreply.github.com>
2022-09-09 19:55:26 +0100

    Update 0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (Success)
+ */
+ 
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+  if ( n > 0 ) 
+  {
+    print ("%d is positive", n);
+  }
+  else if ( n == 0)
+  {
+    print ("%d is zero", n);
+  }
+  else
+  {
+    print ("%d is negative", n);
+  }
+	/* your code goes there */
+	return (0);
+}
