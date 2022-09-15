1b8f529f227776ac00c2320ddd3c5573b1a2f9ad
Adebaicy <66224641+Adebaicy@users.noreply.github.com>
2022-09-15 14:56:56 +0100

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -7,12 +7,12 @@
  */
 int _isupper(int c)
 {
-  if ((c > 0) && (c < 9))
-    {
-      return (1);
-    }
-  else
-    {
-      return (0);
-    }
-}~
+if ((c > 0) && (c < 9))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
